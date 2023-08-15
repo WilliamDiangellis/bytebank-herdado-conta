d038ade6ed4406cdb190e9a4245a60fc0d5c513b
William Diangellis <diangellis2014@gmail.com>
2023-08-15 01:00:45 -0300

    Byte-bank-herdado

---
+++ src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,47 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+//		String nome = "Mario";
+//		nome.replace('o', 'a');
+//		System.out.println(nome);
+		
+//		String nome = "Mario";
+//		nome = nome.replace('o', 'a');
+//		System.out.println(nome);
+
+//		String nome = "Alura";//Object literal
+//		String outro = new String("Alura");//pode utilizar o new mas neste caso é desnecessario
+//		System.out.println(nome + " " + outro);
+
+//		System.out.println(nome.contains("Al"));//retorna true 
+
+//		String vazio = "             ";
+//		System.out.println(vazio.trim().isEmpty());//retorna true pq o trim elimina os espaços tornando um string vazio
+//		
+//		for (int i = 0; i < nome.length(); i++) {
+//			System.out.println(nome.charAt(i));//itera todos os elementos da sting alura retonando o caracter na posição i
+//		}
+//		System.out.println(nome.length());//retona um inteiro sendo o tamanho da string
+
+//		String sub = nome.substring(1);
+//		System.out.println(sub);//retorna parte da string tornando uma substring 
+
+//		int pos = nome.indexOf("ur");
+//		System.out.println(pos);//retorna um inteiro na posição do indice que corresponder 
+
+//		char c = nome.charAt(2);
+//		System.out.println(c);//retorna o caracter que ocupa a posição 2 
+
+		// char c = 'A';
+		// char d = 'a';
+		// String outra = nome.replace(c, d);//retorna a string com o char c substituido
+		// por char d, ou seja troca A por a.
+
+		// String outra = nome.toUpperCase();
+		// System.out.println(nome);//retorna a string maiuscula
+	}
+
+}
